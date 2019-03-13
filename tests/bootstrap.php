45ad5df4a7a72823bd70ae6669604c38a2d1fae8
auooru <auooru@outlook.com>
2019-03-14 00:15:28 +0800

    代码检查与持续集成

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@
  * Date: 2019/2/5
  * Time: 14:47
  */
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 \think\facade\App::instance(dirname(__DIR__));
 \think\facade\App::debug(true);
