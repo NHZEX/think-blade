c40e3f0c9558212441e60529c30043323cd83520
auooru <auooru@outlook.com>
2023-08-13 11:52:42 +0800

    update: 通过第三方库模式引入代码

---
+++ third-party/vendor/composer/installed.php
@@ -0,0 +1,146 @@
+<?php return array(
+    'root' => array(
+        'name' => 'third-party/library',
+        'pretty_version' => '7.x-dev',
+        'version' => '7.9999999.9999999.9999999-dev',
+        'reference' => '80b7aa429e5215663847eeb3f97f1425fc89604d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'doctrine/inflector' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'illuminate/collections' => array(
+            'pretty_version' => 'v9.52.15',
+            'version' => '9.52.15.0',
+            'reference' => 'd3710b0b244bfc62c288c1a87eaa62dd28352d1f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../illuminate/collections',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'illuminate/conditionable' => array(
+            'pretty_version' => 'v9.52.15',
+            'version' => '9.52.15.0',
+            'reference' => 'bea24daa0fa84b7e7b0d5b84f62c71b7e2dc3364',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../illuminate/conditionable',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'illuminate/container' => array(
+            'pretty_version' => 'v9.52.15',
+            'version' => '9.52.15.0',
+            'reference' => '1641dda2d0750b68bb1264a3b37ff3973f2e6265',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../illuminate/container',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'illuminate/contracts' => array(
+            'pretty_version' => 'v9.52.15',
+            'version' => '9.52.15.0',
+            'reference' => '44f65d723b13823baa02ff69751a5948bde60c22',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../illuminate/contracts',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'illuminate/events' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'illuminate/filesystem' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'illuminate/macroable' => array(
+            'pretty_version' => 'v9.52.15',
+            'version' => '9.52.15.0',
+            'reference' => 'e3bfaf6401742a9c6abca61b9b10e998e5b6449a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../illuminate/macroable',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'illuminate/support' => array(
+            'pretty_version' => 'v9.52.15',
+            'version' => '9.52.15.0',
+            'reference' => '223c608dbca27232df6213f776bfe7bdeec24874',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../illuminate/support',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'illuminate/view' => array(
+            'pretty_version' => 'v9.52.15',
+            'version' => '9.52.15.0',
+            'reference' => '0215165781b3269cdbecdfe63ffddd0e6cecfd6e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../illuminate/view',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nesbot/carbon' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'psr/container' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.0.*',
+            ),
+        ),
+        'psr/container-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.1|2.0',
+            ),
+        ),
+        'psr/simple-cache' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '3.0.*',
+            ),
+        ),
+        'symfony/finder' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'symfony/polyfill-php80' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'third-party/library' => array(
+            'pretty_version' => '7.x-dev',
+            'version' => '7.9999999.9999999.9999999-dev',
+            'reference' => '80b7aa429e5215663847eeb3f97f1425fc89604d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'voku/portable-ascii' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
